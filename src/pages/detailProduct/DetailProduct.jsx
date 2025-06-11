c4dc6eb9bec3d49fa5b8fd2f986a823b76eba715
Fredy.Dsd <mfdiony@gmail.com>
2025-06-11 16:15:40 -0300

    feat: se agrego la logica, mas falta darle la estetica correspondiente

--- src/pages/detailProduct/DetailProduct.jsx
+++ src/pages/detailProduct/DetailProduct.jsx
@@ -1,6 +1,24 @@
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
 
 export const DetailProduct = () => {
+    const { id } = useParams();
+
+    const product = useSelector(state =>
+        state.products.find(p => p.id === parseInt(id)));
+    
+    if (!product) {
+        return <div>Producto no encontrado</div>;
+    }   
     return (
-        <div>DetailProduct</div>
+        <div>
+            <h1>{product.title}</h1>
+            <img src={product.image} alt="" />
+            <p>{product.description}</p>
+            <p>{product.category}</p>
+            <p>opiniones de otros usuarios: {product.rating.rate}</p>
+            <p>stock: {product.rating.count}</p>
+        </div>
     )
 }

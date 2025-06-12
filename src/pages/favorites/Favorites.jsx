7ea71306d17f1449e26c41bb1f293920e182442c
rodolfougarte89 <34588024@fi.unju.edu.ar>
2025-06-12 18:37:00 -0300

    se sube pagina de favoritos y componente

--- src/pages/favorites/Favorites.jsx
+++ src/pages/favorites/Favorites.jsx
@@ -1,6 +1,30 @@
 
+import { useSelector } from 'react-redux';
+import CardComponent from '../../components/ui/Card/Card';
+import { Grid, Typography } from '@mui/material';
+import { Title } from '../../components/ui/Title'; //  Titulo Importado
+
 export const Favorites = () => {
+  const products = useSelector((state) => state.products);
+
+  const favorites = products.filter(product => product.favorite);
+
   return (
-    <div>Favorites</div>
-  )
-}
+    <div className="p-4">
+      
+      <Title className="text-green-700 text-center mb-4" text="Mis Favoritos" />
+
+      {favorites.length === 0 ? (
+        <Typography variant="body1">No hay productos favoritos.</Typography>
+      ) : (
+        <Grid container spacing={2}>
+          {favorites.map(product => (
+            <Grid item xs={12} sm={6} md={4} key={product.id}>
+              <CardComponent product={product} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </div>
+  );
+};

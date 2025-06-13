d44beed1d5eaf0d06a2290b4e80425c037c444ba
rodolfougarte89 <34588024@fi.unju.edu.ar>
2025-06-13 03:50:39 -0300

    Se agerga estilos al titulo y un icono. Tambien se agrega estilos y animacion de tailwind css.

--- src/pages/favorites/Favorites.jsx
+++ src/pages/favorites/Favorites.jsx
@@ -1,24 +1,35 @@
-
-import { useSelector } from 'react-redux';
-import CardComponent from '../../components/ui/Card/Card';
-import { Grid, Typography } from '@mui/material';
-import { Title } from '../../components/ui/Title'; //  Titulo Importado
+import { Grid, Box } from "@mui/material"; // Se importa Box en vez de Typography
+import CardComponent from "../../components/ui/Card/Card";
+import { Title } from "../../components/ui/Title";
+import FavoriteTwoToneIcon from '@mui/icons-material/FavoriteTwoTone'; //Nuevo icono de corazon
+import { useSelector } from "react-redux";
 
 export const Favorites = () => {
   const products = useSelector((state) => state.products);
-
-  const favorites = products.filter(product => product.favorite);
+  const favorites = products.filter((product) => product.favorite);
 
   return (
     <div className="p-4">
-      
-      <Title className="text-green-700 text-center mb-4" text="Mis Favoritos" />
+      <div className="flex justify-center mb-4">
+        <Title className="text-2xl xl:text-3xl text-center text-green-700 mb-8" text="Mis Favoritos">
+          <FavoriteTwoToneIcon className="text-green-600" /> {/*Icono corazon al lado del titulo */}
+        </Title>
+      </div>
 
       {favorites.length === 0 ? (
-        <Typography variant="body1">No hay productos favoritos.</Typography>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="300px"
+          className="text-gray-600 text-lg"
+
+        >{/* Se agrega parrafo y se le agrega ANIMACION*/ }
+          <p className="text-2xl font-bold text-gray-400 animate-pulse">¿Sin favoritos? ¡Ponete la 10 y elegí algo!</p> 
+        </Box>
       ) : (
         <Grid container spacing={2}>
-          {favorites.map(product => (
+          {favorites.map((product) => (
             <Grid item xs={12} sm={6} md={4} key={product.id}>
               <CardComponent product={product} />
             </Grid>
